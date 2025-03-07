7947d997d7445b96a5beec7a76e7f22e8a7032d8
KVECKO <samo.knieza@gmail.com>
2025-03-07 12:35:09 +0100

    now i can get 3765 links for apartments

--- sRealityScraper.py
+++ sRealityScraper.py
@@ -9,19 +9,31 @@ page = urlopen(url)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
-# links = []
-
-# for link in soup.findAll("a", class_="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1s6ohwi"):
-#     links.append(base_url+link['href'])
-
 #pages 2 to 164
 pages = range(2,165)
+#all urls are strored here
 urls = []
 urls.append(url)
 #go through the pages
 for page in pages:
     next_page_url = "https://www.sreality.cz/hledani/prodej/byty/praha?strana="+str(page)+"&vlastnictvi=osobni"
     urls.append(next_page_url)
+    print("page=",page)
 
-#all urls are strored here
-print(urls)
+#links for apartments
+links = []
+
+#cycle through all urls 
+page_counter = 1
+link_counter = 1
+for url in urls:
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup2 = BeautifulSoup(html, "html.parser")
+    #cycle through all links to apartments 
+    for link in soup2.find_all("a", class_="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1s6ohwi"):
+        links.append(base_url+link['href'])
+        print("page: ",page_counter,", link: ",link_counter)
+        link_counter = link_counter + 1
+    page_counter = page_counter + 1
+print(len(links))
